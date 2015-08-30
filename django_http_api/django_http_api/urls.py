26d089681b720bff6a89515404e73adaf07ffcde
Kuldeep Fouzdar <kuldeepfouzdar@gmail.com>
2015-08-31 02:56:42 +0530

    Modify views import path

--- django_http_api/django_http_api/urls.py
+++ django_http_api/django_http_api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django_http_api.views import HelloWorldView
+from views import HelloWorldView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
