fca44dd52a309de22c1a9aa9b4c020be707353b1
Vibhor Meshram <vibhor@hashcube.com>
2015-09-14 13:48:03 +0530

    added csrf protection

--- vms/static/javascripts/vms.js
+++ vms/static/javascripts/vms.js
@@ -1,17 +1,20 @@
-(function () {
-  'use strict';
+angular
+  .module('vms', [
+    'vms.routes',
+    'vms.authentication',
+    'vms.config'
+  ]);
 
-  angular
-    .module('vms', [
-      'vms.routes',
-      'vms.authentication',
-      'vms.config'
-    ]);
+angular
+  .module('vms.routes', ['ngRoute']);
 
-  angular
-    .module('vms.routes', ['ngRoute']);
+angular
+  .module('vms.config', []);
 
-  angular
-    .module('vms.config', []);
+angular
+  .module('vms')
+  .run(run)
 
-})();
+function run($http) {
+  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $http.defaults.xsrfCookieName = 'csrftoken';
