6c9c2437027c3255b8b4af86ab3e3b4ee124c67a
Vibhor Meshram <vibhor@hashcube.com>
2015-09-14 15:34:46 +0530

    Added login interface Improved register function

--- vms/static/javascripts/authentication/services/authentication.service.js
+++ vms/static/javascripts/authentication/services/authentication.service.js
@@ -9,16 +9,68 @@
 
   function Authentication($cookies, $http) {
     var Authentication = {
-      register: register
+      register: register,
+      login: login,
+      getAuthenticatedAccount: getAuthenticatedAccount,
+      isAuthenticated: isAuthenticated,
+      setAuthenticatedAccount: setAuthenticatedAccount,
+      unauthenticate: unauthenticate
     };
 
     return Authentication;
 
-    function register(email, password, username) {
+    function register(email, password) {
       return $http.post('/api/v1/accounts/', {
         password: password,
         email: email
-      });
+      }).then(registerSuccessFn, registerErrorFn);
+
+      function registerSuccessFn() {
+        Authenticate.login(email, password);
+      }
+
+      function registerErrorFn() {
+        console.log('MASSIVE ERROR');
+      }
+
+    }
+
+
+    function login(email, password) {
+      return $http.post('/api/v1/auth/login/', {
+        email: email,
+        password: password
+      }).then(loginSuccessFn, loginErrorFn);
+
+      function loginSuccessFn(data, status, headers, config) {
+        Authentication.setAuthenticatedAccount(data.data);
+
+        window.location = '/';
+      }
+
+      function loginErrorFn(data, status, headers, config) {
+        console.log('Something seriously went wrong');
+      }
+    }
+
+    function getAuthenticatedAccount() {
+      if (!$cookies.authenticatedAccount) {
+        return;
+      }
+
+      return JSON.parse($cookies.authenticatedAccount);
+    }
+
+    function isAuthenticated() {
+      return !!$cookies.authenticatedAccount;
+    }
+
+    function setAuthenticatedAccount(account) {
+      $cookies.authenticatedAccount = JSON.stringify(account);
+    }
+
+    function unauthenticate() {
+      delete $cookies.authenticatedAccount;
     }
   }
 })()
