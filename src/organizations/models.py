7e3904bc7611d5751ad0dfbbf91ecfaf40ddd58d
Vibhor Meshram <vibhor@hashcube.com>
2015-09-26 16:04:15 +0530

    added blank=True for certain fields

--- src/organizations/models.py
+++ src/organizations/models.py
@@ -5,8 +5,8 @@ from core.models import TimeStampedModel
 
 class Organization(TimeStampedModel):
     name = models.CharField(max_length=40, unique=True)
-    phone = models.CharField(max_length=20)
-    location = models.CharField(max_length=40)
+    phone = models.CharField(max_length=20, blank=True)
+    location = models.CharField(max_length=40, blank=True)
 
     def __unicode__(self):
         return self.name
