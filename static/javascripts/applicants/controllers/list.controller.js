a16995f444f907d881c67fb02939bdedf51ee802
Vibhor Meshram <vibhormeshram92@gmail.com>
2015-11-30 09:49:08 +0530

    added ng-repeat to stages

--- static/javascripts/applicants/controllers/list.controller.js
+++ static/javascripts/applicants/controllers/list.controller.js
@@ -5,12 +5,14 @@
     .module('vms.applicants.controllers')
     .controller('ApplicantListController', ApplicantListController);
 
-  ApplicantListController.$inject = ['$location', '$routeParams', '$scope', 'Applicants'];
+  ApplicantListController.$inject = ['$location', '$routeParams', '$scope', 'Applicants', 'Stages'];
 
-  function ApplicantListController($location, $routeParams, $scope, Applicants) {
+  function ApplicantListController($location, $routeParams, $scope, Applicants, Stages) {
     var vm = this;
 
     vm.applicants = [];
+
+    vm.stages = [];
 
     vm.id = $routeParams.id;
 
@@ -27,6 +29,17 @@
       $scope.$on('applicant.created.error', function() {
         vm.applicants.shift();
       });
+
+      Stages.all(vm.id)
+        .then(stagesGetSuccessFn, stagesGetErrorFn);
+
+      function stagesGetSuccessFn(data, status, headers, config) {
+        vm.stages = data.data;
+      }
+
+      function stagesGetErrorFn(data, status, headers, config) {
+        console.log('Error while fetching stages in ApplicantListController');
+      }
 
       function applicantListSuccessFn(data, status, headers, config) {
         vm.applicants = data.data;
