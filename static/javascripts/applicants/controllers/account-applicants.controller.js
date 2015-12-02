1eadcf55fc76fc8f3588cb7f025d2966e0b888a9
Vibhor Meshram <vibhormeshram92@gmail.com>
2015-12-02 16:11:28 +0530

    Improved Stages Views

--- static/javascripts/applicants/controllers/account-applicants.controller.js
+++ static/javascripts/applicants/controllers/account-applicants.controller.js
@@ -14,6 +14,10 @@
 
     vm.applicants = [];
 
+    vm.stages = {};
+
+    vm.total = 0;
+
     vm.assignee = Authentication.getAuthenticatedAccount();
 
     if (vm.assignee) {
@@ -23,7 +27,26 @@
       function applicantListSuccessFn(data, status, headers, config) {
         vm.applicants = data.data;
 
-        console.log('SUccess data= ', data.data);
+        for (var applicant in vm.applicants) {
+          Applicants.get(vm.applicants[applicant].id)
+            .then(applicantGetSuccessFn, applicantGetErrorFn);
+
+          function applicantGetSuccessFn(data, status, headers, config) {
+            applicant = data.data;
+
+            if (!vm.stages[applicant.stage.name]) {
+              vm.stages[applicant.stage.name] = {};
+              vm.stages[applicant.stage.name].count = 0;
+              vm.stages[applicant.stage.name].id = applicant.stage.id;
+            }
+            vm.stages[applicant.stage.name].count++;
+            vm.total++;
+          }
+
+          function applicantGetErrorFn(data, status, headers, config) {
+            console.log('Error while getting applicant');
+          }
+        }
       }
 
       function applicantListErrorFn(data, status, headers, config) {
